d2e210aa0f1636672339e108c1d0df1ed39ad2bd
skoczek <jendolf@gmail.com>
2024-07-16 12:53:05 +0200

    init

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('hello there');
+    }
+}
